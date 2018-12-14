9548ddd44932d5a53c7aa5b17fbd9ebd0971799b
selenium guru <selenium.guru.tech@gmail.com>
2018-12-13 23:54:41 -0500

    Updated with css modules

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import './App.css';
-import Radium, { StyleRoot } from 'radium';
 import Person from './Person/Person';
 
 class App extends Component {
@@ -57,11 +56,7 @@ class App extends Component {
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
-      cursor: 'pointer',
-      ':hover': {
-        backgroundColor: 'lightgreen',
-        color: 'black'
-      }
+      cursor: 'pointer'
     };
 
     let persons = null;
@@ -81,10 +76,6 @@ class App extends Component {
          </div>
       );
       style.backgroundColor = 'red';
-      style[':hover'] = {
-        backgroundColor: 'salmon',
-        color: 'black'
-      };
     }
 
     // let classes = ['red', 'bold'].join(' ');
@@ -96,7 +87,6 @@ class App extends Component {
       classes.push('bold'); //classes = ['red', 'bold']
     }
     return (
-      <StyleRoot>
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p className={classes.join(' ')}>This is really working!</p>
@@ -105,7 +95,6 @@ class App extends Component {
          onClick={this.togglePersonHandler}>Toggle Persons</button>
         {persons}
         </div>
-      </StyleRoot>
     );
 
     // return React.createElement('div', null, 'h1', 'Hi I\'m a React App!!!');
@@ -114,4 +103,4 @@ class App extends Component {
   }
 }
 
-export default Radium(App);
+export default App;
