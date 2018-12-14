d7640976cb92c00a85205a941f1a7ad2f6cab895
selenium guru <selenium.guru.tech@gmail.com>
2018-12-13 23:49:54 -0500

    Using media queries without css modules

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import Radium, { StyleRoot } from 'radium';
 import Person from './Person/Person';
 
 class App extends Component {
@@ -36,12 +37,12 @@ class App extends Component {
   //   ]
   //  })
   }
-
+  
   deletePersonHandler = (personIndex) => {
     //const persons = this.state.persons.slice();
     const persons = [...this.state.persons];
     persons.splice(personIndex, 1);
-    this.setState({persons: persons});
+    this.setState({persons: persons}); 
   }
 
   togglePersonHandler = () => {
@@ -51,11 +52,16 @@ class App extends Component {
  
   render() {
     const style = {
-      backgroundColor: 'white',
+      backgroundColor: 'green',
+      color: 'white',
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
-      cursor: 'pointer'
+      cursor: 'pointer',
+      ':hover': {
+        backgroundColor: 'lightgreen',
+        color: 'black'
+      }
     };
 
     let persons = null;
@@ -74,17 +80,32 @@ class App extends Component {
             })}
          </div>
       );
+      style.backgroundColor = 'red';
+      style[':hover'] = {
+        backgroundColor: 'salmon',
+        color: 'black'
+      };
     }
 
+    // let classes = ['red', 'bold'].join(' ');
+    let classes = [];
+    if (this.state.persons.length <= 2){
+      classes.push('red'); //classes = ['red']
+    }
+    if (this.state.persons.length <= 1){
+      classes.push('bold'); //classes = ['red', 'bold']
+    }
     return (
+      <StyleRoot>
       <div className="App">
         <h1>Hi, I'm a React App</h1>
-        <p>This is really working!</p>
+        <p className={classes.join(' ')}>This is really working!</p>
         <button
          style={style}
          onClick={this.togglePersonHandler}>Toggle Persons</button>
         {persons}
         </div>
+      </StyleRoot>
     );
 
     // return React.createElement('div', null, 'h1', 'Hi I\'m a React App!!!');
@@ -93,4 +114,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default Radium(App);
